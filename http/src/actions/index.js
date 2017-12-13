ba074fde0cbddaf775301d353f3a9c8b1aaeb165
atiffany <abby.tiffany@gmail.com>
2017-12-13 14:05:00 -0500

    after guided lecture

--- http/src/actions/index.js
+++ http/src/actions/index.js
@@ -4,4 +4,13 @@ export const GET_FRIENDS = 'GET_FRIENDS';
 
 export const getFriends = () => {
     // Add the code for this action
+    let payload ={};
+
+    const apiUrl = 'http://localhost:5000/friends';
+    const friendsRequest = axios.get(apiUrl);
+
+    return {
+        type: GET_FRIENDS,
+        payload: friendsRequest,
+    }
 };
